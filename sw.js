c47c3bd7c4eff281b539a274d9cc888ff118c388
Mikayilzade <m.mikayil.z@gmail.com>
2026-08-02 15:17:03 +0400

    Refresh PWA cache for Qoşa açdı rules

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='domino-schetki-v3';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='domino-schetki-v4';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./qosha-rules.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()]));});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))));});
